b3a1312900e3482083bb2f1ef25af9e46cbc2aa5
Nguyen Phi <nguyenphi0511@yahoo.com.vn>
2020-02-13 23:49:53 +0700

    add demo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Facebook from './components/Facebook';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Facebook Authentication
+        </p>
+        <Facebook />
+    </div>
+  );
+}
+
+export default App;
